d30f08c10f2e95250292643fb80bc8360228ca3f
ezurno <ezurno@gmail.com>
2022-12-30 21:24:03 +0900

    useInput()
    1. 값을 입력할 때 사용
    
    2. validator 검증 기능 추가

---
+++ UseInputHook.js
@@ -0,0 +1,19 @@
+import { useState } from "react-dom";
+
+export default function UseInputHook(initialValue, validator) {
+  const [value, setValue] = useState(initialValue);
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    let willUpdate = true;
+    if (typeof validator === "function") {
+      willUpdate = validator(value);
+    }
+    if (willUpdate) {
+      setValue(value);
+    }
+  };
+
+  return { value, onChange };
+}
